d697841092c2aeb6967c8b13c1363cb2a8c2bbe0
nishanthan <nishanthan@codevus.com>
2023-02-02 21:28:37 +0530

    last_changes

--- components/FirstComp.jsx
+++ components/FirstComp.jsx
@@ -1,65 +1,20 @@
 import styles from "../styles/FirstCom.module.css";
 import AdCom from "./AdComp";
 import DownloadForOfflineOutlinedIcon from "@mui/icons-material/DownloadForOfflineOutlined";
-import Image from "next/image";
 
 import { products } from "../public/data";
-
-import img1 from "../public/images/img1.png";
-import img2 from "../public/images/img2.png";
-import img3 from "../public/images/img3.png";
-import img5 from "../public/images/img5.png";
 
 const FirstComp = () => {
   return (
     <div className={styles.container}>
       <div className={styles.cardContainer}>
-        {/* {products.map((item) => (
+        {products.map((item) => (
           <AdCom item={item} key={item.id} />
-        ))} */}
-        <din className={styles.firstCard}>
-          <Image src={img1} />
-          <div className={styles.infoContainer}>
-            <p>info</p>
-            <p>price</p>
-          </div>
-        </din>
-
-        <din className={styles.firstCard}>
-          <Image src={img2} />
-          <div className={styles.infoContainer}>
-            <p>info</p>
-            <p>price</p>
-          </div>
-        </din>
-
-        <din className={styles.firstCard}>
-          <Image src={img3} />
-          <div className={styles.infoContainer}>
-            <p>info</p>
-            <p>price</p>
-          </div>
-        </din>
-
-        <din className={styles.firstCard}>
-          <Image src={img2} />
-          <div className={styles.infoContainer}>
-            <p>info</p>
-            <p>price</p>
-          </div>
-        </din>
-
-        <din className={styles.firstCard}>
-          <Image src={img5} />
-          <div className={styles.infoContainer}>
-            <p>info</p>
-            <p>price</p>
-          </div>
-        </din>
+        ))}
       </div>
       <div className={styles.textContainer}>
         <div className={styles.firstSection}>
-          <p className={styles.topText}>Children of </p>
+          <p className={styles.topText}>CHILDREN OF</p>
           <p className={styles.middleText}>FORGOTTEN</p>
           <p className={styles.bottomText}> GOD </p>
         </div>
@@ -70,7 +25,9 @@ const FirstComp = () => {
           </div>
         </div>
       </div>
-      <button className={styles.thirdSection}>START BIDDING</button>
+      <div className={styles.buttonContainer}>
+        <button className={styles.thirdSection}>START BIDDING</button>
+      </div>
     </div>
   );
 };
--- public/data.js
+++ public/data.js
@@ -1,43 +1,61 @@
 import img1 from "../public/images/img1.png";
 import img2 from "../public/images/img2.png";
 import img3 from "../public/images/img3.png";
-import img4 from "../public/images/img4.PNG";
 import img5 from "../public/images/img5.png";
+import img6 from "../public/images/img6.png";
+import img7 from "../public/images/img7.png";
+import img8 from "../public/images/img8.png";
 
 export const products = [
   {
     id: 1,
     img: img1,
     desc: "Id dignissim nulla tortor adipiscing tortor euismod",
-    price: "26200",
-    width: "600px",
+    price: "26,200",
   },
   {
     id: 2,
     img: img2,
-    desc: "Id dignissim nulla tortor adipiscing tortor euismod",
-    price: "26200",
-    width: "500px",
+    desc: "Ut enim donec gravida mollis",
+    price: "7,850",
   },
   {
     id: 3,
     img: img3,
-    desc: "Id dignissim nulla tortor adipiscing tortor euismod",
-    price: "26200",
-    width: "300px",
+    desc: "Tellus vel enim curabitur neque amet amet",
+    price: "10,330",
   },
   {
     id: 4,
     img: img2,
-    desc: "Id dignissim nulla tortor adipiscing tortor euismod",
-    price: "26200",
-    width: "400px",
+    desc: "Ut enim donec gravida",
+    price: "2,150",
   },
   {
     id: 5,
     img: img5,
-    desc: "Id dignissim nulla tortor adipiscing tortor euismod",
-    price: "26200",
-    width: "200px",
+    desc: "Et nulla et facilisi ",
+    price: "2,150",
+  },
+];
+
+export const pics = [
+  {
+    id: 1,
+    img: img6,
+    des: "Ultrices fames neque pharetra",
+    info: "Nulla faucibus",
+  },
+  {
+    id: 2,
+    img: img7,
+    des: "Ultrices fames neque pharetra",
+    info: "Nulla faucibus",
+  },
+  {
+    id: 3,
+    img: img8,
+    des: "Ultrices fames neque pharetra",
+    info: "Nulla faucibus",
   },
 ];
